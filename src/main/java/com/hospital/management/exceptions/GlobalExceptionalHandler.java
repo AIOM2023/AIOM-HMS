01e872c15c52b585f9a9ee2b6ae84ff00df71626
Sundeep <ch.sundeep72561@gmail.com>
2024-09-29 20:39:00 +0800

    Global Exception Handler
    Signed-off-by: Sundeep <ch.sundeep72561@gmail.com>

--- src/main/java/com/hospital/management/exceptions/GlobalExceptionalHandler.java
+++ src/main/java/com/hospital/management/exceptions/GlobalExceptionalHandler.java
@@ -1,35 +1,52 @@
 package com.hospital.management.exceptions;
 
 
+import com.hospital.management.model.GenericResponse;
 import com.hospital.management.payload.ErrorResponse;
 import com.hospital.management.payload.ServiceResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionalHandler {
 
+   /* @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GenericResponse<ErrorResponse>> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        return new ResponseEntity<>(new GenericResponse<>(HttpStatus.BAD_REQUEST.value(),false, "BadRequest", errorResponse), HttpStatus.BAD_REQUEST);
+
+    }*/
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<GenericResponse<ErrorResponse>> handleGenericExceptions(Exception ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        return new ResponseEntity<>(new GenericResponse<>(HttpStatus.BAD_REQUEST.value(),false, "BadRequest", errorResponse), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ServiceResponse> handlerResourceNotFoundException(ResourceNotFoundException ex){
+    public ResponseEntity<ServiceResponse> handlerResourecNotFoundException(ResourceNotFoundException ex){
 
         String message = ex.getMessage();
         ServiceResponse response= ServiceResponse.builder().message(message).success(false).httpStatus(HttpStatus.NOT_FOUND).build();
 
-        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+        return new ResponseEntity<ServiceResponse>(response,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(HmsBusinessException.class)
     public ResponseEntity<ErrorResponse> handleHmsBusinessException(HmsBusinessException ex, WebRequest req){
         ErrorResponse errorResponse = ex.getErrorResponse();
-        return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+        return new ResponseEntity<ErrorResponse>(errorResponse, errorResponse.getHttpStatus());
     }
 
     @ExceptionHandler(DuplicateEntryException.class)

7b89e28b584a2542fd9a8452de202833b0f326a3
Sundeep <ch.sundeep72561@gmail.com>
2024-10-14 20:39:37 +0800

    Company Ifo updated code
    Signed-off-by: Sundeep <ch.sundeep72561@gmail.com>

--- src/main/java/com/hospital/management/service/CompanyInfoService.java
+++ src/main/java/com/hospital/management/service/CompanyInfoService.java
@@ -4,12 +4,15 @@ import com.hospital.management.entities.commom.CompanyInfo;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CompanyInfoService {
 
     CompanyInfo save(MultipartFile logo,String companyName,String tinNo,String companyAddress, String contactNo);
 
-    CompanyInfo update(MultipartFile logo,String companyName,String tinNo,String companyAddress, String contactNo);
+    CompanyInfo update(Long companyId,MultipartFile logo,String companyName,String tinNo,String companyAddress, String contactNo);
 
     List<CompanyInfo> companyInfoList();
+
+    Optional<CompanyInfo> companyInfoListById(Long companyId);
 }
--- src/main/java/com/hospital/management/service/impl/CompanyInfoServiceImpl.java
+++ src/main/java/com/hospital/management/service/impl/CompanyInfoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hospital.management.service.impl;
 
 import com.hospital.management.entities.commom.CompanyInfo;
+import com.hospital.management.exceptions.DuplicateEntryException;
 import com.hospital.management.repositary.CompanyInfoRepo;
 import com.hospital.management.service.CompanyInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyInfoServiceImpl implements CompanyInfoService {
@@ -19,6 +21,11 @@ public class CompanyInfoServiceImpl implements CompanyInfoService {
 
     @Override
     public CompanyInfo save(MultipartFile logo, String companyName, String tinNo, String companyAddress, String contactNo) {
+
+        CompanyInfo companyNameExisting = companyInfoRepo.findByCompanyName(companyName);
+        if (companyNameExisting != null) {
+            throw new DuplicateEntryException(companyName + "already exists.");
+        }
         CompanyInfo companyInfo = new CompanyInfo();
         try {
             companyInfo.setLogo(logo.getBytes());
@@ -34,10 +41,17 @@ public class CompanyInfoServiceImpl implements CompanyInfoService {
     }
 
     @Override
-    public CompanyInfo update(MultipartFile logo, String companyName, String tinNo, String companyAddress, String contactNo) {
+    public CompanyInfo update(Long companyId,MultipartFile logo, String companyName, String tinNo, String companyAddress, String contactNo) {
 
+        CompanyInfo companyNameExisting = companyInfoRepo.findByCompanyName(companyName);
+        if(companyNameExisting != null && !(companyNameExisting.getCompanyId().equals(companyId))) {
+            if (companyNameExisting.getCompanyName().equals(companyName)) {
+                throw new DuplicateEntryException(companyName + "' already exists.");
+            }
+        }
         CompanyInfo companyInfo = new CompanyInfo();
         try {
+            companyInfo.setCompanyId(companyId);
             companyInfo.setLogo(logo.getBytes());
             companyInfo.setCompanyName(companyName);
             companyInfo.setCompanyAddress(companyAddress);
@@ -55,5 +69,10 @@ public class CompanyInfoServiceImpl implements CompanyInfoService {
     @Override
     public List<CompanyInfo> companyInfoList() {
         return companyInfoRepo.findAll();
+    }
+
+    @Override
+    public Optional<CompanyInfo> companyInfoListById(Long companyId) {
+        return companyInfoRepo.findById(companyId);
     }
 }
